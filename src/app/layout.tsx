afb55f4fe5d9ea0bf4aa41744c1dc014bfc72981
almadendj <almadendj@gmail.com>
2024-09-17 23:33:22 +0800

    add wallet connect button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { AppKit } from "@/context/web3modal";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppKit>{children}</AppKit>
       </body>
     </html>
   );
---
+++ src/context/web3modal.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+const projectId: string = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID ?? "";
+
+const ethersAdapter = new EthersAdapter();
+
+const metadata = {
+  name: 'ARData Frontend',
+  description: 'ARData Frontend',
+  url: 'http://localhost:3000',
+  icons: []
+}
+
+createAppKit({
+  adapters: [ethersAdapter],
+  metadata: metadata,
+  networks: [mainnet, arbitrum],
+  projectId,
+})
+
+export function AppKit({ children }: { children?: React.ReactNode }) {
+  return children;
+}
