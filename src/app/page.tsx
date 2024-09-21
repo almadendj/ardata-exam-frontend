77f8f38a792b62cae9eb2c307469adb51915469f
almadendj <almadendj@gmail.com>
2024-09-21 23:55:55 +0800

    add balances loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,51 +2,56 @@
 
 import { WalletProvidersDialog } from "@/components/WalletProvidersDialog";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useBalances } from "@/hooks/useBalances";
 import { useWallet } from "@/hooks/useWallet";
 import { Fragment, useState } from "react";
 
 export default function Home() {
-  const { isConnected, disconnect } = useWallet();
+  const { isConnected, disconnect, address } = useWallet();
+  const { tokenBalance, tokenBalanceLoading } = useBalances();
   const [providersDialogOpen, setProvidersDialogOpen] = useState(false);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-
-        <Card>
-          <CardHeader>
-            <CardTitle>
-              Connect your wallet
-            </CardTitle>
-            <CardDescription>
-              This is for ARData Tech Exam
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            {isConnected ? (
-              <Fragment>
-                <span>
-                  Wallet Connected
-                </span>
+    <Fragment>
+      <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col row-start-2 items-center w-full">
+          <Card className="w-full">
+            <CardHeader className="flex-row justify-between items-start">
+              <div className="flex flex-col gap-2">
+                <CardTitle>
+                  {tokenBalanceLoading ? (
+                    "Loading..."
+                  ) : !isConnected ? (
+                    "Connect your wallet"
+                  ) : (
+                    `${tokenBalance} ARDT`
+                  )}
+                </CardTitle>
+                <CardDescription>
+                  {isConnected && !!address && (
+                    address
+                  )}
+                </CardDescription>
+              </div>
+              {isConnected ? (
                 <Button variant={"destructive"} onClick={() => disconnect()}>
                   Disconnect
                 </Button>
-              </Fragment>
-            ) : (
-              <Button onClick={() => setProvidersDialogOpen(true)}>
-                Connect Wallet
-              </Button>
-            )}
-          </CardContent>
-        </Card>
-
-      </main>
+              ) : (
+                <Button onClick={() => setProvidersDialogOpen(true)}>
+                  Connect Wallet
+                </Button>
+              )}
+            </CardHeader>
+          </Card>
+        </main>
+      </div>
 
       <WalletProvidersDialog
         open={providersDialogOpen}
         setOpen={setProvidersDialogOpen}
       />
-    </div>
+    </Fragment>
   );
 }
--- src/hooks/useBalances.tsx
+++ src/hooks/useBalances.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { createContext, useCallback, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { useWallet } from "./useWallet";
 import { ethers } from "ethers";
 import { contractABI, contractAddress } from "@/lib/contractInfo";
@@ -7,7 +7,9 @@ import { contractABI, contractAddress } from "@/lib/contractInfo";
 type BalancesProps = {
   ethBalance: string;
   tokenBalance: string;
-  updateBalances: (address: string) => Promise<void>
+  updateBalances: (address: string) => void;
+  ethBalanceLoading?: boolean;
+  tokenBalanceLoading?: boolean;
 }
 
 const BalancesContext = createContext<BalancesProps | null>(null);
@@ -22,24 +24,59 @@ export function useBalances() {
 
 export function BalancesProvider({ children }: { children?: React.ReactNode }) {
   const [ethBalance, setEthBalance] = useState("");
+  const [ethLoading, setEthLoading] = useState(false);
   const [tokenBalance, setTokenBalance] = useState("");
-  const ethersProvider = useWallet().ethersProvider;
+  const [tokenLoading, setTokenLoading] = useState(false);
+  const { address, ethersProvider } = useWallet();
 
-  const updateBalances = useCallback(async (address: string) => {
-    if (!!ethersProvider) {
-      const ethBalance = await ethersProvider.getBalance(address);
-      setEthBalance(ethers.formatEther(ethBalance));
+  const updateEthBalance = useCallback(async (address: string) => {
+    try {
+      if (!!ethersProvider) {
+        setEthLoading(true);
+        const ethBalance = await ethersProvider.getBalance(address);
+        setEthBalance(ethers.formatEther(ethBalance));
+      }
+    } catch (e) {
+      // TODO: add better error handling
+      console.error(e);
+    } finally {
+      setEthLoading(false);
+    }
+  }, [ethersProvider, setEthBalance]);
 
+  const updateTokenbalance = useCallback(async (address: string) => {
+    try {
+      setTokenLoading(true);
       const contract = new ethers.Contract(contractAddress, contractABI, ethersProvider);
       const tokenBalance = await contract.balanceOf(address);
       setTokenBalance(ethers.formatEther(tokenBalance));
+    } catch (e) {
+      // TODO: add better error handling
+      console.error(e);
+    } finally {
+      setTokenLoading(false);
     }
+  }, [ethers, ethersProvider, contractAddress, contractABI]);
+
+  const updateBalances = useCallback((address: string) => {
+    updateEthBalance(address);
+
+    updateTokenbalance(address);
   }, [ethersProvider]);
+
+  // fetch balances when page loads
+  useEffect(() => {
+    if (!!address) {
+      updateBalances(address);
+    }
+  }, [updateBalances, address]);
 
   return <BalancesContext.Provider value={{
     ethBalance,
     tokenBalance,
-    updateBalances
+    updateBalances,
+    ethBalanceLoading: ethLoading,
+    tokenBalanceLoading: tokenLoading
   }}>
     {children}
   </BalancesContext.Provider>
