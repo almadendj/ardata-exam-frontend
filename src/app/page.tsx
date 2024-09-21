5b4dbc06aecf17d89cf65e92ea789608ada583d6
almadendj <almadendj@gmail.com>
2024-09-21 23:11:29 +0800

    disconnect button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,30 @@
 import { WalletProvidersDialog } from "@/components/WalletProvidersDialog";
 import { Button } from "@/components/ui/button";
 import { useWallet } from "@/hooks/useWallet";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 export default function Home() {
-  const { isConnected } = useWallet();
+  const { isConnected, disconnect } = useWallet();
   const [providersDialogOpen, setProvidersDialogOpen] = useState(false);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        {isConnected && (
-          <span>
-            Wallet Connected
-          </span>
+        {isConnected ? (
+          <Fragment>
+            <span>
+              Wallet Connected
+            </span>
+            <Button variant={"destructive"} onClick={() => disconnect()}>
+              Disconnect
+            </Button>
+          </Fragment>
+        ) : (
+          <Button onClick={() => setProvidersDialogOpen(true)}>
+            Connect Wallet
+          </Button>
         )}
 
-        <Button onClick={() => setProvidersDialogOpen(true)}>
-          Connect Wallet
-        </Button>
       </main>
 
       <WalletProvidersDialog
