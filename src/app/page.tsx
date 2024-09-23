530c900e3e7931cf4391eefce3bde89c4fced951
almadendj <almadendj@gmail.com>
2024-09-23 14:58:38 +0800

    add backend gas price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import MintTokenCard from "@/components/MintTokenCard";
 import TokensCard from "@/components/TokensCard";
+import { Button } from "@/components/ui/button";
 import WalletDetailsCard from "@/components/WalletDetailsCard";
+import axios from "axios";
 
 export default function Home() {
+  const handleBackendCall = async () => {
+    const gasPrice = await axios.get("http://localhost:8080/get-gas-price")
+    console.log("gas price: ", gasPrice);
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
@@ -10,6 +16,9 @@ export default function Home() {
         <WalletDetailsCard />
         <MintTokenCard />
         <TokensCard />
+        <Button onClick={handleBackendCall}>
+          Get ETH Gas Price
+        </Button>
       </main>
     </div>
   );
