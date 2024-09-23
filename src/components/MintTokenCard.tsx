b226542d543dc7b8a1b740ff479e081a48136b4f
almadendj <almadendj@gmail.com>
2024-09-23 09:21:42 +0800

    add mint token inputs

--- src/components/MintTokenCard.tsx
+++ src/components/MintTokenCard.tsx
@@ -1,17 +1,22 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
 
 export default function MintTokenCard() {
   return (
     <Card className="w-full">
       <CardHeader className="flex-row justify-between items-start">
-
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-1 flex-col gap-2">
           <CardTitle>
             Mint Token
           </CardTitle>
           <CardDescription>Enter the details to mint a token</CardDescription>
         </div>
+        <Input className="flex-1" placeholder="Enter name for token" />
       </CardHeader>
+      <CardContent>
+        <Button className="w-full">Mint Token</Button>
+      </CardContent>
     </Card>
   );
 }
