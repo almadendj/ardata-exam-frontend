1ace629b2f408ddfe6346c89c025743254ae781c
almadendj <almadendj@gmail.com>
2024-09-23 09:31:29 +0800

    add react hook forms

--- src/components/MintTokenCard.tsx
+++ src/components/MintTokenCard.tsx
@@ -1,22 +1,43 @@
+'use client';
 import { Input } from "./ui/input";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  tokenName: string;
+}
 
 export default function MintTokenCard() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <Card className="w-full">
-      <CardHeader className="flex-row justify-between items-start">
-        <div className="flex flex-1 flex-col gap-2">
-          <CardTitle>
-            Mint Token
-          </CardTitle>
-          <CardDescription>Enter the details to mint a token</CardDescription>
-        </div>
-        <Input className="flex-1" placeholder="Enter name for token" />
-      </CardHeader>
-      <CardContent>
-        <Button className="w-full">Mint Token</Button>
-      </CardContent>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CardHeader className="flex-row justify-between items-start">
+          <div className="flex flex-1 flex-col gap-2">
+            <CardTitle>
+              Mint Token
+            </CardTitle>
+            <CardDescription>Enter the details to mint a token</CardDescription>
+          </div>
+          <Input
+            {...register("tokenName", { required: "Please enter a token name" })}
+            className="flex-1"
+            placeholder="Enter name for token"
+            error={errors.tokenName?.message ?? ""}
+          />
+        </CardHeader>
+        <CardContent>
+          <Button className="w-full">Mint Token</Button>
+        </CardContent>
+      </form>
     </Card>
   );
 }
